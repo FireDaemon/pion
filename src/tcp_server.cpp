c03d671135c4326ac21be4a1bea5039ca910753e
klaus triendl <klaus@triendl.eu>
2025-11-09 17:12:46 +0200

    Used some more Standard Library iterator functions instead of boost

--- src/tcp_server.cpp
+++ src/tcp_server.cpp
@@ -186,7 +186,7 @@ void server::start(void)
         before_starting();
 
         // configure the acceptor service
-        for (size_t i = 0, n = boost::size(m_tcp_acceptors); i < n; ++i) {
+        for (size_t i = 0, n = std::size(m_tcp_acceptors); i < n; ++i) {
             PION_LOG_INFO(m_logger, "Starting server on endpoint " << m_endpoints[i]);
 
             try {
@@ -243,7 +243,7 @@ void server::stop(bool wait_until_finished)
             }
         };
         // this terminates any connections waiting to be accepted
-        std::for_each(boost::rbegin(m_tcp_acceptors), boost::rend(m_tcp_acceptors),
+        std::for_each(std::rbegin(m_tcp_acceptors), std::rend(m_tcp_acceptors),
             boost::bind(acceptor_closer(), boost::ref(m_logger), _1));
         
         if (! wait_until_finished) {
@@ -302,7 +302,7 @@ void server::listen(void)
         // prune connections that finished uncleanly
         prune_connections();
 
-        for (size_t i = 0, n = boost::size(m_tcp_acceptors); i < n; ++i)
+        for (size_t i = 0, n = std::size(m_tcp_acceptors); i < n; ++i)
         {
             // alias
             acceptor_t& acceptor = m_tcp_acceptors[i];
@@ -342,7 +342,7 @@ void server::listen_on(boost::asio::ip::tcp::endpoint ep)
         prune_connections();
 
         acceptor_t* acceptor = NULL;
-        for (size_t i = 0, n = boost::size(m_tcp_acceptors); i < n; ++i) {
+        for (size_t i = 0, n = std::size(m_tcp_acceptors); i < n; ++i) {
             if (m_endpoints[i] == ep) {
                 acceptor = &m_tcp_acceptors[i];
                 break;
